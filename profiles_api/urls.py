41937743482b324a96cff05a304556bc802fddf0
Priyanshu Ranjan <priyanshuranjan2020@gmail.com>
2024-07-10 11:16:19 +0530

    complete viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path,include #importing the path function from django.urls
 from profiles_api import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
+
+
 
 #creating the url patterns
 urlpatterns = [
-    path('hello-view/',views.HelloApiView.as_view())
-]
+    path('hello-view/',views.HelloApiView.as_view()),
+    path('',include(router.urls)),#generates a set of urls for the ones that are registered in the router
+    
+    
+] 
 
 
